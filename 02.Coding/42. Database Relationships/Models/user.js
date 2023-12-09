7873632f416203132811519e3de0794ad2618b14
i-am-nikki <nikkimalik355@gmail.com>
2023-12-09 21:51:49 +0530

    Added 42th Folder

---
+++ 02.Coding/42. Database Relationships/Models/user.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+main().then(()=> console.log("connection successful!")).catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/dbRelationships');
+}
+
+const userSchema = new Schema({
+    username: String,
+    addresses: [
+        {
+            _id: false,
+            location: String,
+            city: String, 
+        }
+    ]
+});
+
+const User = mongoose.model("User", userSchema);
+const addUsers = async() =>{
+    let user1 = new User({
+        username: 'Sherlockholmes',
+        addresses: [
+            {
+                location: '221B Baker Street', city: 'London'
+            },
+            {
+                location: 'P35 DownTown', city: 'London'
+            }
+        ]
+    });
+    let result = await user1.save();
+    console.log(result);
+}
+
+addUsers();
